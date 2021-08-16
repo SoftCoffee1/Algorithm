7d0bb846cae0a25ce714a8cb1805b8b4cb617c5b
SoftCoffee <65325476+SoftCoffee1@users.noreply.github.com>
2021-08-16 23:36:44 +0900

    Update 15683 감시.py

--- 백준/15683 감시.py
+++ 백준/15683 감시.py
@@ -147,3 +147,99 @@ for DIRS in product([0,1,2,3], repeat = len(CCTVs)):
 ## 정답 출력
 print(minVal)
         
+    
+################################## 두번째 방법 --> 첫번째 방법의 중복을 보완한 풀이! ##################################
+
+
+"""
+1. 0인 구간들 세기 --> 전체 0에서 cctv가 커버 가능한 여역을 빼면 정답이기 때문!
+2. 각각의 cctv에 대해 커버 가능한 좌표들을 cctvs_case에 집합형태로 저장하기
+3. dfs를 활용해 모든 조합을 체크하기!
+"""
+
+def watch(x, y, direction):
+    ret = set()
+
+    for d in direction:
+        new_x, new_y = x + dx[d], y + dy[d]
+
+        ## 범위를 벗어나면 종료
+        while 0 <= new_x < N and 0 <= new_y < M:
+
+            ## 벽을 만나면 그만 나아가기
+            if office[new_x][new_y] == 6:
+                break
+
+            ## 빈칸이면 퍼져나갈 수 있는 곳이므로 추가하기
+            if office[new_x][new_y] == 0:
+                ret.add((new_x, new_y))
+
+            new_x, new_y = new_x + dx[d], new_y + dy[d]
+
+
+    return ret
+
+
+def dfs(n, watched_set):
+    global max_watched_cnt
+
+    if n == len(cctvs_cases):
+        if max_watched_cnt < len(watched_set):
+            max_watched_cnt = len(watched_set)
+
+        return
+
+    for cctv_cases in cctvs_cases[n]:
+        dfs(n+1, watched_set | cctv_cases)
+
+
+## cctv가 가능한 케이스
+cctvs_cases = []
+
+## 방향은 동서남북 순서
+dx, dy = (-1,0,1,0), (0,1,0,-1)
+
+N, M = map(int, input().rstrip().split())
+office = [list(map(int, input().rstrip().split())) for _ in range(N)]
+
+# 0인 구간들을 세는 변수
+will_watched_cnt = 0
+
+for idx in range(N*M):
+    i, j = divmod(idx, M)
+
+    ## 빈칸이면 개수 세기
+    if office[i][j] == 0:
+        will_watched_cnt += 1
+
+    ## 1번 cctv
+    elif office[i][j] == 1:
+        cctvs_cases.append([watch(i,j,[v]) for v in range(4)])
+
+    ## 2번 cctv
+    elif office[i][j] == 2:
+        cctvs_cases.append([watch(i,j, [v, (v+2)%4]) for v in range(2)])
+
+    ## 3번 cctv
+    elif office[i][j] == 3:
+        cctvs_cases.append([watch(i,j,[v, (v+1)%4]) for v in range(4)])
+
+    ## 4번 cctv
+    elif office[i][j] == 4:
+        cctvs_cases.append([watch(i,j,[v, (v+1)%4, (v+2)%4]) for v in range(4)])
+
+    ## 5번 cctv
+    elif office[i][j] == 5:
+        cctvs_cases.append([watch(i,j,[0,1,2,3])])
+
+
+## 최대 cctv영역 저장하는 변수
+max_watched_cnt = 0
+
+
+## 감시 가능한 최대 영역 개수를 구하는 과정
+dfs(0, set())
+
+## 최소 사각지대 개수
+print(will_watched_cnt - max_watched_cnt)
+
